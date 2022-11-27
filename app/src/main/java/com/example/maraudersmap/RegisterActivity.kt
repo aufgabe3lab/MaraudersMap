a8a5021b1eb4c7da7745af8a46574e6b8c1d68cd
FelixAlexK <107888450+FelixAlexK@users.noreply.github.com>
2022-11-27 11:40:26 +0100

    added addTextChangedListener and function for validation

--- app/src/main/java/com/example/maraudersmap/RegisterActivity.kt
+++ app/src/main/java/com/example/maraudersmap/RegisterActivity.kt
@@ -1,12 +1,18 @@
 package com.example.maraudersmap
 
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.graphics.green
+import androidx.core.widget.addTextChangedListener
 
 /**
  * provides function to register a new user
@@ -32,13 +38,40 @@ class RegisterActivity : AppCompatActivity() {
         loginLink = findViewById(R.id.loginLink_textView)
 
         registerButton.setOnClickListener {
-            if(passwordConfirmation.text.toString() == password.text.toString()){
-                makeToast("${username.text}, ${password.text}", Toast.LENGTH_LONG)
-
+            if(validateRegistration()){
+                makeToast("Successfully", Toast.LENGTH_LONG)
             }else{
-                makeToast("Passwords do not match!", Toast.LENGTH_LONG)
+                if(!validateInput(username.text.toString())){
+                    makeToast("Username is invalid!", Toast.LENGTH_LONG)
+                }else if(!validateInput(password.text.toString())){
+                    makeToast("password is invalid", Toast.LENGTH_SHORT)
+                }else{
+                    makeToast("passwords do not match!", Toast.LENGTH_SHORT)
+                }
             }
         }
+
+        passwordConfirmation.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                if(p0.toString() == password.text.toString() ){
+                    passwordConfirmation.backgroundTintList = ColorStateList.valueOf(Color.GREEN)
+                }else{
+                    passwordConfirmation.backgroundTintList = ColorStateList.valueOf(Color.RED)
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if(!(validateInput(passwordConfirmation.text.toString()) && validateInput(password.text.toString()))){
+                    passwordConfirmation.backgroundTintList = ColorStateList.valueOf(Color.GRAY)
+                }
+            }
+
+        })
 
         loginLink.setOnClickListener {
             switchActivity(LoginActivity::class.java)
@@ -64,4 +97,21 @@ class RegisterActivity : AppCompatActivity() {
         val intent = Intent(this@RegisterActivity, destinationClass)
         startActivity(intent)
     }
+
+    private fun validateInput(inputString: String) : Boolean{
+        if(inputString.isEmpty() || inputString.isBlank()){
+            return false
+        }
+
+        return true
+    }
+
+    private fun validateRegistration(): Boolean{
+       if(validateInput(username.text.toString()) && validateInput(password.text.toString()) && passwordConfirmation.backgroundTintList == ColorStateList.valueOf(Color.GREEN)){
+            return true
+        }
+
+        return false
+    }
+
 }
