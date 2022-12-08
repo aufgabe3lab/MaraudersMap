af93dda888090694644d8ded45fa10e23df4e5b7
FelixAlexK <107888450+FelixAlexK@users.noreply.github.com>
2022-12-08 18:13:24 +0100

    changed sleep value

--- app/src/main/java/com/example/maraudersmap/LoginActivity.kt
+++ app/src/main/java/com/example/maraudersmap/LoginActivity.kt
@@ -61,7 +61,7 @@ class LoginActivity : AppCompatActivity() {
         }
 
         registerLink.setOnClickListener {
-            Thread.sleep(500L)
+            Thread.sleep(1000L)
             switchActivity(SettingsActivity::class.java)
         }
 
